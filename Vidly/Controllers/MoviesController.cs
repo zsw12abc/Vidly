bc5b4a1ce3b02f1076aefd735debd9c9b6505445
Shaowei <Shaowei.Zhang@Outlook.com>
2018-03-28 22:10:43 +1100

    add movie random view and controller

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET MovieS/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name = "Secret"};
+            
+            return View(movie);
+        }
+    }
+}
