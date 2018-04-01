0e45b739b667ae7668724a6fba8a57c9c95520fd
Shaowei <Shaowei.Zhang@Outlook.com>
2018-04-01 21:07:47 +1000

    Revert "Working with data Overriding Conventions"
    This reverts commit 1e73f49eba42b291e099b3cd14d304701efc5941.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,11 +8,6 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
-        [Required]
-        [StringLength(255)]
         public string Name { get; set; }
-        public bool IsSubscribedToNewsletter { get; set; }
-        public MembershipType MembershipType { get; set; }
-        public byte MmebershipTypeId { get; set; }
     }
 }
--- Vidly/Models/IdentityModels.cs
+++ Vidly/Models/IdentityModels.cs
@@ -13,15 +13,13 @@ namespace Vidly.Models
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here 
+            // Add custom user claims here
             return userIdentity;
         }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public DbSet<Customer> Customers { get; set; }
-        
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
