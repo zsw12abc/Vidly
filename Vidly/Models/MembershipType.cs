1e73f49eba42b291e099b3cd14d304701efc5941
Shaowei <Shaowei.Zhang@Outlook.com>
2018-04-01 20:20:12 +1000

    Working with data Overriding Conventions

---
+++ Vidly/Models/MembershipType.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Vidly.Models
+{
+    public class MembershipType
+    {
+        public byte Id { get; set; }
+        public short SignUpFee { get; set; }
+        public byte DurationInMonths { get; set; }
+        public byte DiscountRate { get; set; }
+    }
+}
