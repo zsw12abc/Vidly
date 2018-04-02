5ce1641b350b82c11363b1392e3c0bb8165c37f4
Shaowei <Shaowei.Zhang@Outlook.com>
2018-04-02 12:45:21 +1000

    Markup

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -20,6 +20,11 @@ namespace Vidly.Controllers
             _context.Dispose();
         }
 
+        public ActionResult New()
+        {
+            return View();
+        }
+
         public ViewResult Index()
         {
             var customers = _context.Customers.Include(c => c.MembershipType).ToList();
