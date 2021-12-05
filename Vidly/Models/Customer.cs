e1d0c170231b6b2bfec94d951a6ad1aa81b32a15
Shaowei Zhang <11704698+zsw12abc@users.noreply.github.com>
2021-12-05 16:18:49 +1100

    Overriding Convention

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,9 +1,11 @@
-﻿namespace Vidly.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        [Required] [StringLength(255)] public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
